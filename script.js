b2dabbe983613f93eb39cd0e1c3f065bed7bc3aa
Mister Dumbo <110036209+MrDumbo17@users.noreply.github.com>
2022-08-21 12:35:48 -0300

    versão 1.0

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-var teste =  document.querySelector('#teste')
+var allinp = document.querySelectorAll('.val')
+var realinp = document.querySelectorAll('.valR')
 
 var luxo = document.querySelectorAll('.luxo')
 var simples = document.querySelectorAll('.simples')
 var totalL = document.querySelector('strong#totalL')
 var totalS = document.querySelector('strong#totalS')
+
 
 var valTotalL = 0
 var valTotalS = 0
@@ -11,15 +13,24 @@ var valTotalS = 0
 var valBack = {tipo: '', loc: 0 , valor: 0}
 
 function carregar() {
+   
     luxo.forEach((setL) => setL.value = 0)
     luxo.forEach((valor,i) => valor.addEventListener('change',function(){somaL(i)}))
     luxo.forEach((valor,i) => valor.addEventListener('focus',function(){clicar('l',i)}))
     luxo.forEach((valor) => valor.addEventListener('blur',focusout))
-    
+
+
     simples.forEach((setS) => setS.value = 0)
-    simples.forEach((valor,i) => valor.addEventListener('change',function(){somaL(i)}))
+    simples.forEach((valor,i) => valor.addEventListener('change',function(){somaS(i)}))
     simples.forEach((valor,i) => valor.addEventListener('focus',function(){clicar('s',i)}))
     simples.forEach((valor) => valor.addEventListener('blur',focusout))
+
+    allinp.forEach((valor,i) => valor.addEventListener('focus',function(){clicar('i',i)}))
+    allinp.forEach((valor) => valor.addEventListener('blur',focusout))
+
+    realinp.forEach((valor,i) => valor.addEventListener('focus',function(){clicar('r',i)}))
+    realinp.forEach((valor) => valor.addEventListener('blur',focusout))
+    realinp.forEach((valor,i) => valor.addEventListener('change',function(){alt(i)}))
     
 }
  
@@ -36,6 +47,16 @@ function clicar(type, loc){
         valBack.loc = loc
         valBack.valor = simples[loc].value
         simples[loc].value = ''
+    }else if (type == 'i'){
+        valBack.tipo = 'input'
+        valBack.loc = loc
+        valBack.valor = allinp[loc].value
+        allinp[loc].value = ''
+    }else if (type == 'r'){
+        valBack.tipo = 'real'
+        valBack.loc = loc
+        valBack.valor = realinp[loc].value
+        realinp[loc].value = ''
     }
 
     
@@ -43,16 +64,19 @@ function clicar(type, loc){
     
 }
 function focusout(){
-    
-   
-    
 
     if(valBack.tipo == 'luxo' && luxo[valBack.loc].value == ''){
         luxo[valBack.loc].value = valBack.valor
     }else if (valBack.tipo == 'simples' && simples[valBack.loc].value == ''){
 
-        simples[valBack.loc] = valBack.valor
-    }    
+        simples[valBack.loc].value = valBack.valor
+    }else if (valBack.tipo == 'input' && allinp[valBack.loc].value == ''){
+
+        allinp[valBack.loc].value = valBack.valor
+    }else if (valBack.tipo == 'real' && realinp[valBack.loc].value == ''){
+
+        realinp[valBack.loc].value = valBack.valor
+    }
     
 }
 function somaL(x = 0) {
@@ -63,6 +87,15 @@ function somaL(x = 0) {
         valTotalL += Number(luxo[i].value)
     }
     totalL.innerHTML = valTotalL
+    
+    if(valTotalL == 113){
+
+        totalL.style.color = 'green'
+
+    }else{
+
+        totalL.style.color = 'red'
+    }
 }
 function somaS(x = 0) {
 
@@ -72,5 +105,19 @@ function somaS(x = 0) {
         valTotalS += Number(simples[i].value)
     }
     totalS.innerHTML = valTotalS
+
+    if(valTotalS == 46){
+
+        totalS.style.color = 'green'
+
+    }else{
+
+        totalS.style.color = 'red'
+    }
+}
+function alt(x=0){
+   
+
+        realinp[x].value = Number(realinp[x].value).toFixed(2)
 }
     
