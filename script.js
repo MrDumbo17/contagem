07162406e428a465571d9d73b577a11b5b5bf702
Mister Dumbo <110036209+MrDumbo17@users.noreply.github.com>
2022-08-21 11:00:43 -0300

    .

--- script.js
+++ script.js
@@ -1,25 +1,76 @@
-var luxo = document.getElementsByClassName('luxo')
+var teste =  document.querySelector('#teste')
+
+var luxo = document.querySelectorAll('.luxo')
+var simples = document.querySelectorAll('.simples')
 var totalL = document.querySelector('strong#totalL')
+var totalS = document.querySelector('strong#totalS')
 
 var valTotalL = 0
+var valTotalS = 0
 
-for(let i in luxo){
+var valBack = {tipo: '', loc: 0 , valor: 0}
 
-    luxo[i].addEventListener('change',function(){soma(i)},false)
+function carregar() {
+    luxo.forEach((setL) => setL.value = 0)
+    luxo.forEach((valor,i) => valor.addEventListener('change',function(){somaL(i)}))
+    luxo.forEach((valor,i) => valor.addEventListener('focus',function(){clicar('l',i)}))
+    luxo.forEach((valor) => valor.addEventListener('blur',focusout))
+    
+    simples.forEach((setS) => setS.value = 0)
+    simples.forEach((valor,i) => valor.addEventListener('change',function(){somaL(i)}))
+    simples.forEach((valor,i) => valor.addEventListener('focus',function(){clicar('s',i)}))
+    simples.forEach((valor) => valor.addEventListener('blur',focusout))
     
 }
-
  
+function clicar(type, loc){
 
-function soma(x = 0) {
+    if(type == 'l'){
+        valBack.tipo = 'luxo'
+        valBack.loc = loc
+        valBack.valor = luxo[loc].value
+
+        luxo[loc].value = ''
+    }else if (type == 's'){
+        valBack.tipo = 'simples'
+        valBack.loc = loc
+        valBack.valor = simples[loc].value
+        simples[loc].value = ''
+    }
+
+    
+   
+    
+}
+function focusout(){
+    
+   
+    
+
+    if(valBack.tipo == 'luxo' && luxo[valBack.loc].value == ''){
+        luxo[valBack.loc].value = valBack.valor
+    }else if (valBack.tipo == 'simples' && simples[valBack.loc].value == ''){
+
+        simples[valBack.loc] = valBack.valor
+    }    
+    
+}
+function somaL(x = 0) {
 
     valTotalL = 0
-
-    for(let i in luxo)
+    for(let i = 0; i < --luxo.length; i++)
     {
-        valTotalL = Number(valTotalL) + Number(luxo[i].value)
+        valTotalL += Number(luxo[i].value)
     }
-    
     totalL.innerHTML = valTotalL
+}
+function somaS(x = 0) {
+
+    valTotalS = 0
+    for(let i = 0; i < --simples.length; i++)
+    {
+        valTotalS += Number(simples[i].value)
+    }
+    totalS.innerHTML = valTotalS
 }
     
---
+++ teste.js
@@ -0,0 +1,6 @@
+var a = [5,3,14]
+
+
+a.forEach((el,b) => console.log(el +' '+ b))
+
+
